e2cc62905fae3af7fb7c19354e63d209d6b6f5eb
Zharkyn Kassenov <zharkyn@process.st>
2021-05-06 21:55:38 +0600

    adjusted image quality

--- src/services/conversion.service.ts
+++ src/services/conversion.service.ts
@@ -6,7 +6,11 @@ const convertByFilePath = async (filePath: string) => {
   const convertApi = new ConvertAPI(process.env.CONVERT_API_SECRET as string, {
     conversionTimeout: 60,
   });
-  const result = await convertApi.convert("jpg", { File: filePath });
+  const result = await convertApi.convert("jpg", { 
+    File: filePath,
+    ImageResolution: 100,
+    ImageQuality: 30
+  });
   const paths = new Array<string>();
 
   await result.files.forEach((file) => {
